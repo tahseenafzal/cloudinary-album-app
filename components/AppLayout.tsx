b5618d24a6457caea2e9e0a8fc31791b1eae7a8d
Tehseen Afzal <abuashja@gmail.com>
2023-08-18 07:53:15 +0500

    Update AppLayout.tsx
    Adjust grid-rows

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -3,13 +3,13 @@ import Navbar from "./Navbar";
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-[200px,1fr] grid-rows-3 min-h-screen">
-      <div className="col-span-full">
+    <div className="grid grid-cols-1 lg:grid-cols-[200px,1fr] grid-rows-[auto,1fr,auto] min-h-screen">
+      <div className="col-span-2">
         <Navbar />
       </div>
-      <div className="hidden lg:block">SideBar</div>
-      <div>{children}</div>
-      <div className="col-span-full">
+      <div className="hidden lg:block p-3">SideBar</div>
+      <div className="p-3">{children}</div>
+      <div className="col-span-2 p-3">
         Developed by: TEHSEEN AFZAL - IT Mate Pakistan
       </div>
     </div>
