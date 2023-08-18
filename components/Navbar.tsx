cf16e085c2555eff0a1a465460bda86ecad2f065
Tehseen Afzal <abuashja@gmail.com>
2023-08-18 07:52:47 +0500

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,15 +4,13 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 const Navbar = () => {
   return (
     <div className="border-b">
-      <div className="container">
-        <div className="flex h-16 items-center px-4">
-          PHOTO APP
-          <div className="ml-auto flex items-center space-x-4">
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </div>
+      <div className="flex h-16 items-center px-4 container">
+        PHOTO APP
+        <div className="ml-auto flex items-center space-x-4">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
         </div>
       </div>
     </div>
