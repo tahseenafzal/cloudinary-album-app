58c6591bd3c7efb39226d28340407283a75a58a3
Tehseen Afzal <abuashja@gmail.com>
2023-08-24 08:08:22 +0500

    Update AppLayout.tsx
    Render sidebar

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -7,8 +8,10 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="col-span-2">
         <Navbar />
       </div>
-      <div className="hidden lg:block p-3">SideBar</div>
-      <div className="p-3">{children}</div>
+      <div className="hidden lg:block p-3">
+        <Sidebar />
+      </div>
+      <div className="p-8">{children}</div>
       <div className="col-span-2 p-3">
         Developed by: TEHSEEN AFZAL - IT Mate Pakistan
       </div>
