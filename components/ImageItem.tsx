06e43be16f39bb91b7ed7d5d579dd5ae5d09c70e
Tehseen Afzal <abuashja@gmail.com>
2023-08-25 23:54:09 +0500

    Create ImageItem.tsx

---
+++ components/ImageItem.tsx
@@ -0,0 +1,18 @@
+import { CldImage } from "next-cloudinary";
+import React from "react";
+
+const ImageItem = ({ imageId }: { imageId: string }) => {
+  return (
+    <div className="col-span-full">
+      <CldImage
+        width="200"
+        height="300"
+        src={imageId}
+        sizes="100vw"
+        alt="Description of my image"
+      />
+    </div>
+  );
+};
+
+export default ImageItem;
