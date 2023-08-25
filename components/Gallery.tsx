d2ba2e51bc1cb3ac137addf2c32e0e9cc04f0b2f
Tehseen Afzal <abuashja@gmail.com>
2023-08-25 23:54:03 +0500

    Update Gallery.tsx
    Create UploadButton and ImageItem component and render in this component and make it a server component

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,16 +1,16 @@
 "use client";
-import { CldImage, CldUploadButton } from "next-cloudinary";
 import { useState } from "react";
-import { Button } from "./ui/button";
+import ImageItem from "./ImageItem";
+import UploadButton from "./UploadButton";
+import cloudinary from "cloudinary";
 
-type uploadResult = {
-  info: {
-    public_id: string;
-  };
-  event: "success";
-};
-
-const Gallery = () => {
+const Gallery = async () => {
+  const res = await cloudinary.v2.search
+    .expression("resource_type:image")
+    .sort_by("public_id", "desc")
+    .max_results(30)
+    .execute()
+    .then((result) => console.log(result));
   const [imageId, setImageId] = useState("");
   return (
     <div className="grid grid-cols-[1fr,auto] grid-rows-[auto,1fr] gap-4">
@@ -18,41 +18,9 @@ const Gallery = () => {
         <h1 className="text-lg font-bold">Gallery</h1>
       </div>
       <div>
-        <Button className="flex gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-            />
-          </svg>
-          <CldUploadButton
-            onUpload={(result) => {
-              const imgResult = result as uploadResult;
-              setImageId(imgResult.info.public_id);
-            }}
-            uploadPreset="glbc5qws"
-          />
-        </Button>
+        <UploadButton setImageId={setImageId} />
       </div>
-      <div className="col-span-full">
-        {imageId && (
-          <CldImage
-            width="200"
-            height="300"
-            src={imageId}
-            sizes="100vw"
-            alt="Description of my image"
-          />
-        )}
-      </div>
+      {imageId && <ImageItem imageId={imageId} />}
     </div>
   );
 };
