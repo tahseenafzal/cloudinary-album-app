f06f5a119a0e751b0d77be8b8ee70f483c72614f
Tehseen Afzal <abuashja@gmail.com>
2023-08-27 23:54:02 +0500

    Update Gallery.tsx
    Get Images data and render in gallery component

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,27 +1,35 @@
-"use client";
-import { useState } from "react";
 import ImageItem from "./ImageItem";
 import UploadButton from "./UploadButton";
 import cloudinary from "cloudinary";
 
+interface imageData {
+  public_id: string;
+}
+
 const Gallery = async () => {
-  const result = await cloudinary.v2.search
+  const results = await cloudinary.v2.search
     .expression("resource_type:image")
-    .sort_by("create_at", "desc")
+    .sort_by("created_at", "desc")
     .max_results(30)
     .execute();
 
-  const [imageId, setImageId] = useState("");
+  const imageData = results.resources;
 
   return (
-    <div className="grid grid-cols-[1fr,auto] grid-rows-[auto,1fr] gap-4">
-      <div>
-        <h1 className="text-lg font-bold">Gallery</h1>
+    <div>
+      <div className="grid grid-cols-[1fr,auto] grid-rows-[auto,1fr] gap-4">
+        <div>
+          <h1 className="text-lg font-bold">Gallery</h1>
+        </div>
+        <div>
+          <UploadButton />
+        </div>
       </div>
-      <div>
-        <UploadButton setImageId={setImageId} />
+      <div className="columns-4 space-y-4 gap-3">
+        {imageData.map((item: imageData) => (
+          <ImageItem key={item.public_id} imageId={item.public_id} />
+        ))}
       </div>
-      {imageId && <ImageItem imageId={imageId} />}
     </div>
   );
 };
