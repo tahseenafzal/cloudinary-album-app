b0953a67c14336e32361e6be71cb65cd12567df7
Tehseen Afzal <abuashja@gmail.com>
2023-09-08 23:39:22 +0500

    Update ImageItem.tsx
    Add fill heart and change icon on click on heart

--- components/ImageItem.tsx
+++ components/ImageItem.tsx
@@ -3,8 +3,10 @@ import { CldImage } from "next-cloudinary";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { FiMenu } from "react-icons/fi";
 import { Button } from "./ui/button";
+import { useState } from "react";
 
 const ImageItem = ({ imageId }: { imageId: string }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
   return (
     <div className="relative flex justify-center">
       <CldImage
@@ -16,10 +18,24 @@ const ImageItem = ({ imageId }: { imageId: string }) => {
         alt="Description of my image"
       />
       <div className="flex justify-between absolute top-3 w-[90%] ">
-        <AiOutlineHeart
-          size={32}
-          className="text-red-500 hover:text-red-400 duration-300"
-        />
+        <Button
+          variant={"ghost"}
+          size={"icon"}
+          className="hover:bg-transparent"
+          onClick={() => setIsFavorite(!isFavorite)}
+        >
+          {isFavorite ? (
+            <AiFillHeart
+              size={32}
+              className="text-red-500 hover:text-red-400 duration-300"
+            />
+          ) : (
+            <AiOutlineHeart
+              size={32}
+              className="text-red-500 hover:text-red-400 duration-300"
+            />
+          )}
+        </Button>
         <Button variant="secondary" size="icon">
           <FiMenu size={18} />
         </Button>
